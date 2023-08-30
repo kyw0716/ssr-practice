70e77ae00985a7aa992cf6b675aa69af7c3707a6
kyw0716 <kyw0716@naver.com>
2023-08-30 11:25:33 +0900

    refactor: 서버 코드 파일 네이밍 변경

--- src/server.tsx
+++ src/server.tsx
@@ -1,3 +1,5 @@
+import { stationIdStore } from './client/stores/stationIdStore';
+
 const express = require('express');
 const ReactDOMServer = require('react-dom/server');
 const App = require('./client/App').default;
@@ -7,6 +9,28 @@ const app = express();
 app.use(express.static('dist'));
 
 app.get('/', (req, res) => {
+  stationIdStore.setState(null);
+  const app = ReactDOMServer.renderToString(<App />);
+
+  const template = `
+    <html>
+        <head>
+            <title>SSR 연습</title>
+        </head>
+        <body>
+            <div id="root">${app}</div>
+            <script src="bundle.js"></script>
+        </body>
+    </html>
+  `;
+
+  res.send(template);
+});
+
+app.get('/test/:id', (req, res) => {
+  const id = req.params.id;
+  stationIdStore.setState(id);
+
   const app = ReactDOMServer.renderToString(<App />);
 
   const template = `
--- webpack.server.js
+++ webpack.server.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   target: 'node',
-  entry: './src/index.tsx',
+  entry: './src/server.tsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'server'),
